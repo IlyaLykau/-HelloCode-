3682de837af117b4d07be1dea8e428dd42eb4ac5
IlyaLykau <viperliv@gmail.com>
2023-02-25 12:02:55 +0300

    add info main

---
+++ seminar001/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num2 == num1/num2)
+{
+Console.WriteLine("Первое число - это квадрат второго числа!");
+}
+else 
+Console.WriteLine("Первое число - это не квадрат второго числа!");
